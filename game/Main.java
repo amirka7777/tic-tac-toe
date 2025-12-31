03f603446006468ec9b536b184f9665648b6c155
amirka7777 <amirmgoian0808@gmail.com>
2025-12-31 15:15:59 +0700

    added makeMove()

---
+++ game/Main.java
@@ -0,0 +1,85 @@
+package game;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    static void printBoard() {
+            System.out.println(" 0  1  2");
+            for (int i = 0; i < 3; i++) {
+                System.out.print(i + " ");
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j]);
+                    if (j < 2) {
+                        System.out.print("|");
+                    }
+                }
+                System.out.println();
+                if (i < 2) {
+                    System.out.println(" -------");
+                }
+            }
+        }
+
+        static void initializationBoard() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j ++) {
+                    board[i][j] = ' ';
+                }
+            }
+        }
+
+        static void makeMove() {
+            
+            boolean trueMove = false;
+
+            while (!trueMove) {
+                System.out.println("очередь игрока - " + correctPlayer);
+                System.out.print("Введите ваш столбец: ");
+                int col = scanner.nextInt();
+                System.out.print("введите вашу строчку: ");
+                int line = scanner.nextInt();
+
+                if (col < 0 || col > 2 || line < 0 || line > 2) {
+                    System.out.println("неверный выбор координат");
+                    continue;
+                }
+
+                if (board[line][col] != ' ') {
+                    System.out.println("клетка уже занята");
+                    continue;
+                }
+
+                board[line][col] = correctPlayer;
+                trueMove = true;
+
+                if (correctPlayer == 'X') {
+                    correctPlayer = 'O';
+                }
+                else {
+                    correctPlayer = 'X';
+                }
+            }
+
+            
+
+        }
+
+    private static char[][] board = new char[3][3];
+    private static char correctPlayer = 'X';
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        initializationBoard();
+        printBoard();
+
+        while (true) {
+            makeMove();
+
+            printBoard();
+        }
+
+    }
+}
